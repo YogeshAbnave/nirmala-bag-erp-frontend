769402b551227d5425e914575af6bf6c013235c2
Yogesh Abnave <iamyogeshabnave@gmail.com>
2024-11-10 01:39:25 +0530

    resolved form error

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,17 @@ import { AppRoutingModule } from './app.routes';
 import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 // import { ToastrModule } from 'ngx-toastr';
 @NgModule({
  
     imports: [
+      // BrowserAnimationsModule,
+      NoopAnimationsModule,
       AppRoutingModule,
       BrowserModule,
       ReactiveFormsModule,
       HttpClientModule,
-      BrowserAnimationsModule,
     ],
   declarations: [
   ],
